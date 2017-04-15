76c1e500181cf101ab835c89e9276abbf6fd080e
Brian Buckley <briankbuckley@gmail.com>
2017-04-15 18:48:27 -0400

    add cool

--- src/reducers/index.spec.js
+++ src/reducers/index.spec.js
@@ -4,6 +4,6 @@ describe('index', () => {
   it('should handle initial state', () => {
     let state = null;
     state = index(null, { type: SET_PBC, name: 'pbc' });
-    expect(state).toEqual('pbc');
+    expect(state).toEqual('pbc'); //cool
   });
 });
