ae7e9375f110a634b9b439589f64c74ffc7e8ce0
brian buckley <briankbuckley@gmail.com>
2017-05-15 14:22:00 -0400

    add persist

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import index from './reducers/index';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-//const NAME = 'learning';
-// if (!localStorage.getItem(NAME)) {
-//   localStorage.setItem(NAME, JSON.stringify({}));
-// }
+const NAME = 'learning';
+if (!localStorage.getItem(NAME)) {
+  localStorage.setItem(NAME, JSON.stringify({}));
+}
 
 export const store = createStore(
   index,
-  //    JSON.parse(localStorage.getItem(NAME))
+      JSON.parse(localStorage.getItem(NAME))
 );
 
 ReactDOM.render(
@@ -30,5 +30,5 @@ ReactDOM.render(
 
 store.subscribe(() => {
   //console.log(NAME, "am writing to local", JSON.stringify(store.getState()));
-  //localStorage.setItem(NAME, JSON.stringify(store.getState()));
+  localStorage.setItem(NAME, JSON.stringify(store.getState()));
 });
