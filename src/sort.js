3096e68246d7bb99286fea48834ee0e8da65104f
brian buckley <briankbuckley@gmail.com>
2017-06-02 21:37:34 -0400

    stable

--- src/sort.js
+++ src/sort.js
@@ -1,15 +1,7 @@
-//const m = this.props.sorter
-// const r = (m[0] === '!')
-// const field = (r  ? m.substr(1) : m)
-
-// const convertUnicode = input =>
-//   input.replace(/\\u(\w{4,4})/g, (a, b) =>
-//     String.fromCharCode(parseInt(b, 16)),
-//   );
+import { fields } from './fields';
 
 export const symbol = (current, { fld, order }) =>
   current !== fld ? current : order ? fld + '\u2193' : fld + '\u2191';
-
 
 export const sort_date_field = (a, b, x = 'crd', r = false) => {
   let a1 = a[x];
@@ -62,17 +54,81 @@ export const sort_text_field = (a, b, x = 'text', r = false) => {
 };
 
 export const sorter = (tcs, { fld, order }) => {
-  return tcs.sort((a, b) => {
-    let a1 = a[fld];
-    let b1 = b[fld];
-    if (order) {
-      if (a1 < b1) return -1;
-      if (a1 > b1) return 1;
-      return sort_text_field(a, b, 'id', order);
-    } else {
-      if (a1 < b1) return 1;
-      if (a1 > b1) return 11;
-      return sort_text_field(a, b, 'id', !order);
-    }
-  });
+  let sort;
+  switch (fields[fld].sort_type) {
+    case 'text':
+      sort = (a, b) => {
+        const z = '';
+        if (typeof a[fld] === 'undefined') {
+          a[fld] = z;
+        }
+        if (typeof b[fld] === 'undefined') {
+          b[fld] = z;
+        }
+        let a1 = a[fld];
+        let b1 = b[fld];
+        if (order) {
+          if (a1 < b1) return -1;
+          if (a1 > b1) return 1;
+          return sort_text_field(a, b, 'id', order);
+        } else {
+          if (a1 < b1) return 1;
+          if (a1 > b1) return -1;
+          return sort_text_field(a, b, 'id', !order);
+        }
+      };
+      break;
+    case 'date':
+      sort = (a, b) => {
+        let a1 = a[fld];
+        let b1 = b[fld];
+        let r1 = '1/1/1900';
+        if (order === false) {
+          r1 = '12/31/9999';
+        }
+        if (a1 === undefined || a1 === '') {
+          a1 = r1;
+        }
+        if (b1 === undefined || b1 === '') {
+          b1 = r1;
+        }
+        a1 = new Date(a1);
+        b1 = new Date(b1);
+        if (order) {
+          if (a1 < b1) return -1;
+          if (a1 > b1) return 1;
+          return 0;
+        } else {
+          if (a1 < b1) return 1;
+          if (a1 > b1) return -1;
+          return 0;
+        }
+      };
+      break;
+    case 'number':
+      sort = (a, b) => {
+        const z = '';
+        if (typeof a[fld] === 'undefined') {
+          a[fld] = z;
+        }
+        if (typeof b[fld] === 'undefined') {
+          b[fld] = z;
+        }
+        let a1 = a[fld];
+        let b1 = b[fld];
+        if (order) {
+          if (Number(a1) < Number(b1)) return -1;
+          if (Number(a1) > Number(b1)) return 1;
+          return sort_text_field(a, b, 'id', order);
+        } else {
+          if (Number(a1) < Number(b1)) return 1;
+          if (Number(a1) > Number(b1)) return -1;
+          return sort_text_field(a, b, 'id', !order);
+        }
+      };
+      break;
+    default:
+  }
+
+  return tcs.sort(sort);
 };
